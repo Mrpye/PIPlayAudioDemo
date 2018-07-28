e21985bb9b8ef9d9656c5e0131ccbbde5a2cefc5
DESKTOP-04SGI81\Andrewp <andrewp@metsi.co.uk>
2018-07-28 10:10:24 +0100

    First Import

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import pygame
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.IN)
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('Your mp3 file goes here')
+buttondown=False
+play=False
+print "Start"
+
+try:
+
+    while True:
+        if(GPIO.input(21)==False):
+            if(Play==False and buttondown==False):
+                #Play Audio
+                pygame.mixer.music.rewind()
+                pygame.mixer.music.stop()
+                pygame.mixer.music.play()
+                buttondown=True
+            elif(Play==True and buttondown==False):
+                #Stop Audio
+                pygame.mixer.music.stop()
+                pygame.mixer.music.rewind()
+                buttondown=True
+        elif(buttondown==True):
+            buttondown=False
+except KeyboardInterrupt:
+        pass
